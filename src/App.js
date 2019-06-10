5f08ff9a39e9027d3f2b4901ffa585d8aee49c30
Jonathan Sexton <joncsexton@.com>
2019-06-10 16:33:12 -0500

    instantiated quip object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-// const Quip = require('Quip');
-// const quipApp = new Quip({
-//     // Quip Access Token (required)
-//     accessToken: 'QVJNQU1BZHJOVHo=|1591737067|4qcahbUMtC9BBSJUJx4RXfHhD+/oguWLLD+jvbJZYx4='
-// });
+import Quip from 'quip.js';
+
+const quipApp = new Quip({
+    // Quip Access Token (required)
+    accessToken: 'QVJNQU1BZHJOVHo=|1591737067|4qcahbUMtC9BBSJUJx4RXfHhD+/oguWLLD+jvbJZYx4='
+});
 
 function App() {
+  console.log(quipApp)
   return (
     <div className="App">
       Is this working?

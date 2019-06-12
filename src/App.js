09720d635558ab5baf8d93effc2f162327d4d3a7
Jonathan Sexton <joncsexton@gmail.com>
2019-06-12 11:17:05 -0500

    working on post function

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import React, { Component } from "react";
-import Quip from "quip.js";
-import axios from "axios";
+import React, { Component } from 'react';
+import Quip from 'quip.js';
+import axios from 'axios';
 
-const makePostRequest = async () => {
-  try {
-    const options = {
-      accessToken: 'QVJNQU1BZHJOVHo=|1591737067|4qcahbUMtC9BBSJUJx4RXfHhD+/oguWLLD+jvbJZYx4=',
-      headers: {
-        
-
-      } 
-
-    }
-    let response = axios.post("https://platform.quip.com/1/threads/new-document", options);
-    console.log(response.status);
-  } catch (error) {
-    console.error(`POST Error: ${error}`);
-  }
+const makePostRequest = () => {
+  fetch('https://platform.quip.com/1/threads/new-document', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      'Authorization': 'QVJNQU1BZHJOVHo=|1591737067|4qcahbUMtC9BBSJUJx4RXfHhD+/oguWLLD+jvbJZYx4=',
+    },
+    body: {
+      title: 'POST TEST 2',
+      format: 'html',
+    },
+  });
 };
+
+// const makePostRequest = async () => {
+//   try {
+//     const options = {
+//       accessToken: 'QVJNQU1BZHJOVHo=|1591737067|4qcahbUMtC9BBSJUJx4RXfHhD+/oguWLLD+jvbJZYx4=',
+//       headers: {
+//         'Content-Type': 'multipart/form-data',
+//         Authorization: 'Bearer QVJNQU1BZHJOVHo=|1591737067|4qcahbUMtC9BBSJUJx4RXfHhD+/oguWLLD+jvbJZYx4=',
+//       },
+//       body: {
+//         title: 'POST TEST 2',
+//         format: 'html',
+//       },
+//     };
+//     let response = await axios.post('https://platform.quip.com/1/threads/new-document', options);
+//     console.log(response.status);
+//   } catch (error) {
+//     console.error(`POST Error: ${error}`);
+//   }
+// };
 
 const quipApp = new Quip({
   // Quip Access Token (required)
-  accessToken: "QVJNQU1BZHJOVHo=|1591737067|4qcahbUMtC9BBSJUJx4RXfHhD+/oguWLLD+jvbJZYx4=",
+  accessToken: 'QVJNQU1BZHJOVHo=|1591737067|4qcahbUMtC9BBSJUJx4RXfHhD+/oguWLLD+jvbJZYx4=',
 });
 
 class App extends Component {

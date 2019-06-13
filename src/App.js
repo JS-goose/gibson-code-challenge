8e33f472ce4cff26316fb1ff963f3f88b96932b5
Jonathan Sexton <joncsexton@gmail.com>
2019-06-13 05:34:19 -0500

    finally got the post function to work correctly!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import qs from 'qs';
 import Nav from './components/Nav/Nav';
 
+const makePostRequest = () => {
+  const body = qs.stringify({
+    title: 'React App',
+    type: 'spreadsheet',
+    format: 'html',
+    content: 'This is a test of the POST function',
+  });
+
+  fetch('https://cors-anywhere.herokuapp.com/https://platform.quip.com/1/threads/new-document', {
+    method: 'post',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      authorization:
+        'Bearer QVJNQU1Bc2VKaWE=|1591916456|ZAKGDHexrykpcdkGjULyvJF771+UBUf2jB1dGQdTW9w=',
+    },
+    body,
+  })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
 // const makePostRequest = () => {
-//   fetch('https://cors-anywhere.herokuapp.com/https://platform.quip.com/1/threads/new-document', {
-//     method: 'post',
+// axios.post(
+//   'https://cors-anywhere.herokuapp.com/https://platform.quip.com/1/threads/new-document',
+//   {
 //     headers: {
 //       'content-type': 'application/x-www-form-urlencoded',
 //       authorization:
@@ -16,29 +43,33 @@ import Nav from './components/Nav/Nav';
 //       format: 'html',
 //       content: 'This is a test of the POST function',
 //     },
-//   })
-//     .then((response) => {
-//       console.log(response);
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
+//   }
+// );
+// const body = qs.stringify({
+//   title: 'React App',
+//   type: 'spreadsheet',
+//   format: 'html',
+//   content: 'This is a test of the POST function',
+// });
+
+// const headers = {
+//   'content-type': 'application/x-www-form-urlencoded',
+//   authorization:
+//     'Bearer QVJNQU1Bc2VKaWE=|1591916456|ZAKGDHexrykpcdkGjULyvJF771+UBUf2jB1dGQdTW9w=',
 // };
-
-const makePostRequest = () => {
-  axios.post({
-    method: 'post',
-  })
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-};
-
-
-
+// axios
+//   .post(
+//     'https://cors-anywhere.herokuapp.com/https://platform.quip.com/1/threads/new-document',
+//     body,
+//     headers
+//   )
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+// };
 
 class App extends Component {
   render() {

491bf0b597e8bcf1e4564e0ad052fbdfb90290aa
Jonathan Sexton <joncsexton@.com>
2019-06-13 11:31:51 -0500

    working on putting the footer at bottom

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './Footer.module.css';
 
 const Footer = () => {
     return (
-        <div>
+        <footer>
             <p>This is the footer</p>
-        </div>
+        </footer>
     )
 }
 

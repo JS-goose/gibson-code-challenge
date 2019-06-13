9b028e357819d31e3668b1e07e8ad6fc46cb7fd5
Jonathan Sexton <joncsexton@.com>
2019-06-13 16:45:56 -0500

    moved POST function to center content

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import qs from 'qs';
 import Nav from './components/Nav/Nav';
 import CenterContent from './components/CenterContent/CenterContent';
 import Footer from './components/Footer/Footer';
-
-const makePostRequest = () => {
-  const body = qs.stringify({
-    title: 'React App',
-    type: 'spreadsheet',
-    format: 'html',
-    content: 'This is a test of the POST function',
-  });
-
-  fetch('https://cors-anywhere.herokuapp.com/https://platform.quip.com/1/threads/new-document', {
-    method: 'post',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      authorization:
-        'Bearer QVJNQU1Bc2VKaWE=|1591916456|ZAKGDHexrykpcdkGjULyvJF771+UBUf2jB1dGQdTW9w=',
-    },
-    body,
-  })
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Nav />
-        <CenterContent makePostRequest={makePostRequest}/>
+        <CenterContent/>
         <Footer />
       </div>
     );
--- src/components/CenterContent/CenterContent.js
+++ src/components/CenterContent/CenterContent.js
@@ -1,11 +1,38 @@
 import React, { Component }from 'react';
+import qs from 'qs';
 
 class CenterContent extends Component {
+    makePostRequest = () => {
+        const body = qs.stringify({
+          title: 'React App',
+          type: 'spreadsheet',
+          format: 'html',
+          content: 'This is a test of the POST function',
+        });
+      
+        fetch('https://cors-anywhere.herokuapp.com/https://platform.quip.com/1/threads/new-document', {
+          method: 'post',
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            authorization:
+              'Bearer QVJNQU1Bc2VKaWE=|1591916456|ZAKGDHexrykpcdkGjULyvJF771+UBUf2jB1dGQdTW9w=',
+          },
+          body,
+        })
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      };
+
     render() {
         return (
             <div>
                 CenterContent
-                <button onClick={this.props.makePostRequest}>POST REQUEST</button>
+                <button onClick={this.makePostRequest}>POST REQUEST</button>
+                <button onClick={this.getThreads}>GET THREADS</button>
             </div>
         )
     }

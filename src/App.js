3e688807ef706e662135784623f6ba6dfdd89b09
Jonathan Sexton <joncsexton@gmail.com>
2019-06-13 04:43:16 -0500

    uninstalled axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 const makePostRequest = () => {
   // const accessToken = 'QVJNQU1Bc2VKaWE=|1591916456|ZAKGDHexrykpcdkGjULyvJF771+UBUf2jB1dGQdTW9w=';
@@ -25,34 +24,7 @@ const makePostRequest = () => {
     });
 };
 
-// const makePostRequest = async () => {
-//   try {
-//     const accessToken = 'QVJNQU1Bc2VKaWE=|1591916456|ZAKGDHexrykpcdkGjULyvJF771+UBUf2jB1dGQdTW9w=';
-//     await fetch(
-//       'https://cors-anywhere.herokuapp.com/https://platform.quip.com/1/threads/new-document',
-//       {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/x-www-form-urlencoded',
-//           Authorization: `Bearer ${accessToken}`,
-//         },
-//         form: {
-//           title: 'React App',
-//           type: 'spreadsheet',
-//           format: 'html',
-//           content: 'This is a test of the POST function',
-//         },
-//       }
-//     );
-//   } catch (error) {
-//     console.error(`POST Error: ${error}`);
-//   }
-// };
 
-// const quipApp = new Quip({
-//   // Quip Access Token (required)
-//   accessToken: 'QVJNQU1BZHJOVHo=|1591737067|4qcahbUMtC9BBSJUJx4RXfHhD+/oguWLLD+jvbJZYx4=',
-// });
 
 class App extends Component {
   render() {

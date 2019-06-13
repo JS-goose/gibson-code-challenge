25e2f31e982e088a53fba5badf6f36769bf7a19a
Jonathan Sexton <joncsexton@gmail.com>
2019-06-13 05:41:16 -0500

    removed unused post function

--- src/App.js
+++ src/App.js
@@ -28,49 +28,6 @@ const makePostRequest = () => {
     });
 };
 
-// const makePostRequest = () => {
-// axios.post(
-//   'https://cors-anywhere.herokuapp.com/https://platform.quip.com/1/threads/new-document',
-//   {
-//     headers: {
-//       'content-type': 'application/x-www-form-urlencoded',
-//       authorization:
-//         'Bearer QVJNQU1Bc2VKaWE=|1591916456|ZAKGDHexrykpcdkGjULyvJF771+UBUf2jB1dGQdTW9w=',
-//     },
-//     body: {
-//       title: 'React App',
-//       type: 'spreadsheet',
-//       format: 'html',
-//       content: 'This is a test of the POST function',
-//     },
-//   }
-// );
-// const body = qs.stringify({
-//   title: 'React App',
-//   type: 'spreadsheet',
-//   format: 'html',
-//   content: 'This is a test of the POST function',
-// });
-
-// const headers = {
-//   'content-type': 'application/x-www-form-urlencoded',
-//   authorization:
-//     'Bearer QVJNQU1Bc2VKaWE=|1591916456|ZAKGDHexrykpcdkGjULyvJF771+UBUf2jB1dGQdTW9w=',
-// };
-// axios
-//   .post(
-//     'https://cors-anywhere.herokuapp.com/https://platform.quip.com/1/threads/new-document',
-//     body,
-//     headers
-//   )
-//   .then((response) => {
-//     console.log(response);
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
-// };
-
 class App extends Component {
   render() {
     return (

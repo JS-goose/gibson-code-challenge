c81cc70ac5ec37d4c88152992e064a8e74128743
Jonathan Sexton <joncsexton@gmail.com>
2019-06-13 05:51:48 -0500

    added nav links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,9 +2,19 @@ import React from 'react';
 import styles from './Nav.module.css';
 
 const Nav = (props) => {
+  const navLinks = [
+    <a href="#">Home</a>,
+    <a href="#">About</a>,
+    <a href="#">Stats</a>,
+    <a href="#">Contact</a>,
+  ];
   return (
     <nav>
-      <p>this is the nav</p>
+      <ul>
+        {navLinks.map((item,key) => {
+          return <li key={key}>{item}</li>;
+        })}
+      </ul>
     </nav>
   );
 };

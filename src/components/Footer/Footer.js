5077d73dac10a7969d9d236a7e4db6ec49b7fd46
Jonathan Sexton <joncsexton@gmail.com>
2019-06-19 05:17:59 -0500

    added footer text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,12 @@ import React from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => {
-    return (
-        <footer>
-            <p>This is the footer</p>
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <p>MPL#41005 | Employee Portal</p>
+      <p>© 2018 Gibson Plumbing</p>
+    </footer>
+  );
+};
 
 export default Footer;

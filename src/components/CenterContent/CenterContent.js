a8029e6ef8c2c24e51e4aa6d1f860a581d96dea2
Jonathan Sexton <joncsexton@.com>
2019-06-20 13:51:18 -0500

    added handleImport function

--- src/components/CenterContent/CenterContent.js
+++ src/components/CenterContent/CenterContent.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import qs from 'qs';
-import axios from 'axios';
-import styles from './CenterContent.module.css';
+import React, { Component } from "react";
+import qs from "qs";
+import axios from "axios";
+import styles from "./CenterContent.module.css";
 
-const userID = 'ARMAEAXXOQQ';
+const userID = "ARMAEAXXOQQ";
 
 class CenterContent extends Component {
   makePostRequest = () => {
     const body = qs.stringify({
-      title: 'React App',
-      type: 'spreadsheet',
-      format: 'html',
-      content: 'This is a test of the POST function',
+      title: "React App",
+      type: "spreadsheet",
+      format: "html",
+      content: "This is a test of the POST function",
     });
 
-    fetch('https://cors-anywhere.herokuapp.com/https://platform.quip.com/1/threads/new-document', {
-      method: 'post',
+    fetch("https://cors-anywhere.herokuapp.com/https://platform.quip.com/1/threads/new-document", {
+      method: "post",
       headers: {
-        'content-type': 'application/x-www-form-urlencoded',
+        "content-type": "application/x-www-form-urlencoded",
         authorization:
-          'Bearer QVJNQU1BQVFrYmU=|1592341477|0BjZDfHHvNc9YhMWAp6/bIXZODod9ClZc6fYrBA8K3U=',
+          "Bearer QVJNQU1BQVFrYmU=|1592341477|0BjZDfHHvNc9YhMWAp6/bIXZODod9ClZc6fYrBA8K3U=",
       },
       body,
     })
@@ -31,42 +31,35 @@ class CenterContent extends Component {
       });
   };
 
-  // getThreads = () => {
-  //     const header = {
-  //       'Content-Type': 'application/json; charset=UTF-8',
-  //       Authorization: 'Bearer QVJNQU1BQVFrYmU=|1592341477|0BjZDfHHvNc9YhMWAp6/bIXZODod9ClZc6fYrBA8K3U=',
-  //       'Grant-Type': 'Authorization Code'
-  //     }
-
-  //     axios.get('https://cors-anywhere.herokuapp.com/https://platform.quip.com/1/oauth/login', header
-  //     )
-  //     .then((response) => {
-  //         console.log(response)
-  //     }).catch((error) => {
-  //         console.error(`Error in fetching threads: ${error}`)
-  //     })
-  // }
+  handleImport = (event) => {
+    console.log(event.target.files[0]);
+  };
 
   getUsers = () => {};
 
   render() {
     return (
       <React.Fragment>
-        <article id={styles['center-content-container']}>
-          <div id={styles['spreadsheet-container']}>spreadsheet container</div>
-          <div id={styles['recent-files-container']}>recent files</div>
+        <article id={styles["center-content-container"]}>
+          <div id={styles["spreadsheet-container"]}>spreadsheet container</div>
+          <div id={styles["recent-files-container"]}>recent files</div>
         </article>
-        <div id={styles['controls-container']}>
-          <button className={styles['control-buttons']} onClick={this.makePostRequest}>
+        <div id={styles["controls-container"]}>
+          <button className={styles["control-buttons"]} onClick={this.makePostRequest}>
             Create New Spreadsheet
           </button>
-          <button className={styles['control-buttons']} onClick={this.getThreads}>
+          <button className={styles["control-buttons"]} onClick={this.getThreads}>
             Recent Spreadsheets
           </button>
           <label htmlFor="import-spreadsheet-button">Import Spreadsheet</label>
-          <input type="file" name="imported-spreadsheet" id="import-spreadsheet-button" />
-          <button className={styles['control-buttons']}>Export Current</button>
-          <button className={styles['control-buttons']}>Delete Current</button>
+          <input
+            type="file"
+            name="imported-spreadsheet"
+            id="import-spreadsheet-button"
+            onChange={this.handleImport}
+          />
+          <button className={styles["control-buttons"]}>Export Current</button>
+          <button className={styles["control-buttons"]}>Delete Current</button>
         </div>
       </React.Fragment>
     );
